4596d0ec56f5cc09b51e564145dbcbd6bce48529
JustAHippo <82006314+JustAHippo@users.noreply.github.com>
2022-01-29 12:26:39 -0500

    Create NSFWAllow.plugin.js

---
+++ NSFWAllow.plugin.js
@@ -0,0 +1,28 @@
+/**
+ * @name NSFWAllow
+ * @version 69.420
+ * @description Lets you view NSFW channels despite age
+ * @author Drowzee
+ */
+
+
+
+module.exports = class Example{
+
+        constructor(){
+            this.gaming = true;
+        }
+
+
+        load() { }
+        start() {
+                var findModule=(item)=>window.webpackChunkdiscord_app.push([[Math.random()],{},(req)=>{for(const m of Object.keys(req.c).map((x)=>req.c[x].exports).filt>
+                this.gaming = findModule('getCurrentUser').getCurrentUser().nsfwAllowed;
+                findModule('getCurrentUser').getCurrentUser().nsfwAllowed = true;
+        }
+
+        stop(){
+                var findModule=(item)=>window.webpackChunkdiscord_app.push([[Math.random()],{},(req)=>{for(const m of Object.keys(req.c).map((x)=>req.c[x].exports).filt>
+                findModule('getCurrentUser').getCurrentUser().nsfwAllowed = this.gaming;
+        }
+}
